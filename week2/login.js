50a6dfc9e797ba5a761454d3a5d72e2a12e83280
Ting <tinalin912@gmail.com>
2024-01-19 18:15:34 +0800

    week 2 token

--- week2/login.js
+++ week2/login.js
@@ -20,7 +20,7 @@ createApp({
           const { token, expired } = response.data;
           console.log(token, expired);
           // 儲存token: 在六角提供的MDN網站找，expired需轉型
-          document.cookie = `hextoken=${token}; expires=${new Date(expired)}; path=/`;
+          document.cookie = `hexToken=${token}; expires=${new Date(expired)}; path=/`;
           window.location = 'products.html';
         })
         .catch((err) => {
@@ -33,3 +33,5 @@ createApp({
     },
   },
 }).mount("#app");
+
+// eyJhbGciOiJSUzI1NiIsImtpZCI6ImxrMDJBdyJ9.eyJpc3MiOiJodHRwczovL3Nlc3Npb24uZmlyZWJhc2UuZ29vZ2xlLmNvbS92dWUtY291cnNlLWFwaSIsImF1ZCI6InZ1ZS1jb3Vyc2UtYXBpIiwiYXV0aF90aW1lIjoxNzA1NjU3NDE5LCJ1c2VyX2lkIjoia1pDeEVqM2JFaVNrUG5FQXJ4RE1RRklnNzB2MiIsInN1YiI6ImtaQ3hFajNiRWlTa1BuRUFyeERNUUZJZzcwdjIiLCJpYXQiOjE3MDU2NTc0MTksImV4cCI6MTcwNjA4OTQxOSwiZW1haWwiOiJsaWFyYTA0MDRAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJmaXJlYmFzZSI6eyJpZGVudGl0aWVzIjp7ImVtYWlsIjpbImxpYXJhMDQwNEBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.jPZNM5DHNV59dfLGEVWGMYieLXigk8rsucfVsx-zlRJ757u9HxvcgH8LP05aXnJHiZ3CYBLsrI5Xeiio7WvdSqnB7ted9bHFOY8nw9CDaEzd46WtxGPDAQd4E-pOrXmyfRuKSq679S3yHn6YPEmdavwWc2gFNbq-YOa5Vpu7MXOexRrCiVs7BqzJwHO6Hfaz_ssE-zjdik9m23NybLf51CRFCswmkm6GGw1g04jzZVgSFS6cQOfbfmOvJ8jBbzNhMFnWuguRnHLhzOZ1gM0wpfi74N07Ph-PqfqmZ_C7LD8PitJLzK2VaNF-OPlvdN2rReRZI3oTvd57uRj6dpmtYQ
--- week2/products.js
+++ week2/products.js
@@ -1,6 +1,4 @@
 // 產品資料格式
-// import {axios} from 'axios';
-// Vue.prototype.$axios = axios;
 import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
 
 createApp({
@@ -70,18 +68,20 @@ createApp({
           this.getData();
         })
         .catch((err) => {
-          alert(err.data.message);
+          // console.log(err);
+          alert(err.response.data.message);
           window.location = 'login.html';
         });
     },
     getData(){
-      axios.get(`${this.apiUrl}/api/${this.api_path}/admin/products`)
+      axios.get(`${this.apiUrl}/api/${this.apiPath}/admin/products`)
       .then((response) => {
           console.log(response);
           this.products = response.data.products;
         })
       .catch((err) => { 
-        alert(err.data.message);
+        // console.log(err);
+        alert(err.response.data.message);
       })
     },
     openProduct(item) {
@@ -92,7 +92,8 @@ createApp({
     // 取出 Token
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
     axios.defaults.headers.common.Authorization = token;
+    console.log(token);
 
-    this.checkAdmin()
+    this.checkAdmin();
   }
 }).mount('#app');
